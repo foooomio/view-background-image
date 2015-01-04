615a25859b97be3968189d7e4c2d8286b1aacc27
foooomio <foooomio@gmail.com>
2015-01-04 18:50:21 +0900

    Change permission <all_urls> and refactor the code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var code = "[ getComputedStyle(document.body).getPropertyValue('background-image'),  location.href ]";
+var code = "[ getComputedStyle(document.body).getPropertyValue('background-image'),  location.href ]"; 
 
 chrome.contextMenus.create({
 	id: "background_img",
@@ -8,23 +8,21 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	var url = info.frameUrl || info.pageUrl;
 	
-	chrome.tabs.executeScript(
-		null,
-		{code: code, allFrames: true},
+	chrome.tabs.executeScript(null, {code: code, allFrames: true},
 		function(results) {
-			var i, result, flag = false;
+			var flag = false;
 			
-			for(i = 0; result = results[i]; i++) {
-				if(result[1] === url && result[0].slice(0,3) === "url") {
-					chrome.tabs.create({
-						index: tab.index + 1,
-						url: result[0].slice(4,-1)
-					});
-					flag = true;
-				}
-			}
+			results.forEach(function(result) {console.log(result);
+				if(result[0].slice(0,3) !== "url" || result[1] !== url) return;
+				
+				chrome.tabs.create({
+					index: tab.index + 1,
+					url: result[0].slice(4,-1)
+				});
+				flag = true;
+			});
 			
-			if(flag === false) {
+			if(!flag) {
 				alert("背景画像を取得できません。");
 			}
 		}

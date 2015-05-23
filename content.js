858e5ce90a5fc02489dc65446e40e5a60410485e
foooomio <foooomio@gmail.com>
2015-05-23 23:58:50 +0900

    Add a feature to report issues

--- content.js
+++ content.js
@@ -1,20 +1,21 @@
 function getBackgroundImage(element) {
 	while(element) {
 		var bg = getComputedStyle(element).getPropertyValue('background-image');
-		if(bg !== "none") return bg;
+		if(bg !== "none") return bg.slice(4,-1);
 		element = element.parentElement;
 	}
-	return "none";
 }
 
-var element, listener = function(e) { element = e.target; };
+var target, listener = function(e) { target = e.target; };
+
 document.addEventListener('contextmenu', listener);
+
 Array.prototype.forEach.call(frames, function(frame) {
 	frame.document.addEventListener('contextmenu', listener);
 });
 
 chrome.runtime.onMessage.addListener(
 	function(message, sender, sendResponse) {
-		sendResponse(getBackgroundImage(element));
+		sendResponse(getBackgroundImage(target));
 	}
 );
--- script.js
+++ script.js
@@ -5,13 +5,17 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	chrome.tabs.sendMessage(tab.id, '', function(response) {
-		if(response && response !== "none") {
+		if(response) {
 			chrome.tabs.create({
 				index: tab.index + 1,
-				url: response.slice(4,-1)
+				url: response
 			});
-		} else {
-			alert(chrome.i18n.getMessage('failure'));
+		} else if(confirm(chrome.i18n.getMessage('failure'))) {
+			var xhr, url = encodeURIComponent(info.frameUrl || info.pageUrl);
+			
+			xhr = new XMLHttpRequest();
+			xhr.open('GET', 'https://script.google.com/macros/s/AKfycbwtacVYsZHc_n4qkVdfvjJE_3__rKwfeqTNNgZKPhH00VoKTAA/exec?url=' + url);
+			xhr.send();
 		}
 	});
 });

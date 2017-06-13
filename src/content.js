63b8963630350005d239955fb35b352577f86664
foooomio <foooomio@gmail.com>
2017-06-13 14:26:43 +0900

    Change whitespaces

--- src/content.js
+++ src/content.js
@@ -1,40 +1,40 @@
 function getBackgroundImage(element) {
-	var bg = getComputedStyle(element).getPropertyValue('background-image');
-	if(bg !== "none" && bg.match(/url\("?(.+?)"?\)/)) return RegExp.$1;
+    var bg = getComputedStyle(element).getPropertyValue('background-image');
+    if(bg !== "none" && bg.match(/url\("?(.+?)"?\)/)) return RegExp.$1;
 }
 
 function getNormalImage(element) {
-	if(element.tagName === 'IMG') return element.src;
+    if(element.tagName === 'IMG') return element.src;
 }
 
 function getImages(element, x, y) {
-	var images = [];
-	var elements = element.elementsFromPoint(x, y);
-	for(var i = 0, max = elements.length; i < max; i++) {
-		if(elements[i].shadowRoot) images = images.concat(getImages(elements[i].shadowRoot, x, y));
-		var image = getBackgroundImage(elements[i]) || getNormalImage(elements[i]);
-		if(image) images.push(image);
-	}
-	return images;
+    var images = [];
+    var elements = element.elementsFromPoint(x, y);
+    for(var i = 0, max = elements.length; i < max; i++) {
+        if(elements[i].shadowRoot) images = images.concat(getImages(elements[i].shadowRoot, x, y));
+        var image = getBackgroundImage(elements[i]) || getNormalImage(elements[i]);
+        if(image) images.push(image);
+    }
+    return images;
 }
 
 var IDENTIFIER = 'view-background-image:';
 
 document.addEventListener('contextmenu', function(e) {
-	window.top.postMessage(IDENTIFIER + getImages(document, e.clientX, e.clientY).join(' '), '*');
+    window.top.postMessage(IDENTIFIER + getImages(document, e.clientX, e.clientY).join(' '), '*');
 });
 
 if(window.self === window.top) {
-	var images;
-	window.addEventListener('message', function(e) {
-		if(e.data.indexOf && e.data.indexOf(IDENTIFIER) === 0) {
-			images = e.data.slice(IDENTIFIER.length);
-		}
-	});
-	chrome.runtime.onMessage.addListener(
-		function(message, sender, sendResponse) {
-			sendResponse(images);
-		}
-	);
+    var images;
+    window.addEventListener('message', function(e) {
+        if(e.data.indexOf && e.data.indexOf(IDENTIFIER) === 0) {
+            images = e.data.slice(IDENTIFIER.length);
+        }
+    });
+    chrome.runtime.onMessage.addListener(
+        function(message, sender, sendResponse) {
+            sendResponse(images);
+        }
+    );
 }
 
--- src/script.js
+++ src/script.js
@@ -1,25 +1,25 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.contextMenus.create({
-		contexts: ['all'],
-		id: 'background_img',
-		title: chrome.i18n.getMessage('title')
-	});
+    chrome.contextMenus.create({
+        contexts: ['all'],
+        id: 'background_img',
+        title: chrome.i18n.getMessage('title')
+    });
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	chrome.tabs.sendMessage(tab.id, '', function(response) {
-		if(response) {
-			response.split(' ').forEach(function(image) {
-				chrome.tabs.create({
-					index: tab.index + 1,
-					url: image
-				});
-			});
-		} else if(confirm(chrome.i18n.getMessage('failure'))) {
-			var xhr, url = encodeURIComponent(info.frameUrl || info.pageUrl);
-			xhr = new XMLHttpRequest();
-			xhr.open('GET', 'https://script.google.com/macros/s/AKfycbwtacVYsZHc_n4qkVdfvjJE_3__rKwfeqTNNgZKPhH00VoKTAA/exec?url=' + url);
-			xhr.send();
-		}
-	});
+    chrome.tabs.sendMessage(tab.id, '', function(response) {
+        if(response) {
+            response.split(' ').forEach(function(image) {
+                chrome.tabs.create({
+                    index: tab.index + 1,
+                    url: image
+                });
+            });
+        } else if(confirm(chrome.i18n.getMessage('failure'))) {
+            var xhr, url = encodeURIComponent(info.frameUrl || info.pageUrl);
+            xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://script.google.com/macros/s/AKfycbwtacVYsZHc_n4qkVdfvjJE_3__rKwfeqTNNgZKPhH00VoKTAA/exec?url=' + url);
+            xhr.send();
+        }
+    });
 });

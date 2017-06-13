ea949f55b4830ea73a8845dfe8b632b863657981
foooomio <foooomio@gmail.com>
2017-06-13 18:21:51 +0900

    Refactor

--- src/content.js
+++ src/content.js
@@ -1,40 +1,58 @@
-function getBackgroundImage(element) {
-    var bg = getComputedStyle(element).getPropertyValue('background-image');
-    if(bg !== "none" && bg.match(/url\("?(.+?)"?\)/)) return RegExp.$1;
-}
+//
+// content.js
+//
 
-function getNormalImage(element) {
-    if(element.tagName === 'IMG') return element.src;
-}
+'use strict';
 
-function getImages(element, x, y) {
-    var images = [];
-    var elements = element.elementsFromPoint(x, y);
-    for(var i = 0, max = elements.length; i < max; i++) {
-        if(elements[i].shadowRoot) images = images.concat(getImages(elements[i].shadowRoot, x, y));
-        var image = getBackgroundImage(elements[i]) || getNormalImage(elements[i]);
-        if(image) images.push(image);
+Object.defineProperty(Node.prototype, 'getImages', {
+    value: function(x, y) {
+        const elements = this.elementsFromPoint(x, y)
+            .filter(node => node instanceof Element);
+        return elements.reduce((images, element) => {
+            const image = element.getBackgroundImage()
+                        || element.getNormalImage();
+            if (image) images.push(image);
+            if (element.shadowRoot) {
+                images = images.concat(element.shadowRoot.getImages(x, y));
+            }
+            return images;
+        }, []);
     }
-    return images;
-}
-
-var IDENTIFIER = 'view-background-image:';
-
-document.addEventListener('contextmenu', function(e) {
-    window.top.postMessage(IDENTIFIER + getImages(document, e.clientX, e.clientY).join(' '), '*');
 });
 
-if(window.self === window.top) {
-    var images;
-    window.addEventListener('message', function(e) {
-        if(e.data.indexOf && e.data.indexOf(IDENTIFIER) === 0) {
+Object.defineProperties(Element.prototype, {
+    getBackgroundImage: {
+        value: function() {
+            const style = getComputedStyle(this);
+            const bgimage = style.getPropertyValue('background-image');
+            if (bgimage !== 'none' && bgimage.match(/url\("?(.+?)"?\)/)) {
+                return RegExp.$1;
+            }
+        }
+    },
+    getNormalImage: {
+        value: function() {
+            if (this.tagName === 'IMG') return this.src;
+        }
+    }
+});
+
+const IDENTIFIER = 'view-background-image:';
+
+document.addEventListener('contextmenu', e => {
+    const message = document.getImages(e.clientX, e.clientY).join(' ');
+    window.top.postMessage(IDENTIFIER + message, '*');
+});
+
+if (window.self === window.top) {
+    let images;
+    window.addEventListener('message', e => {
+        if (e.data.indexOf && e.data.indexOf(IDENTIFIER) === 0) {
             images = e.data.slice(IDENTIFIER.length);
         }
     });
-    chrome.runtime.onMessage.addListener(
-        function(message, sender, sendResponse) {
-            sendResponse(images);
-        }
-    );
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        sendResponse(images);
+    });
 }
 
--- src/script.js
+++ src/script.js
@@ -1,4 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
+//
+// script.js
+//
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         contexts: ['all'],
         id: 'background_img',
@@ -6,18 +12,18 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    chrome.tabs.sendMessage(tab.id, '', function(response) {
-        if(response) {
-            response.split(' ').forEach(function(image) {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    chrome.tabs.sendMessage(tab.id, '', response => {
+        if (response) {
+            response.split(' ').map(image => {
                 chrome.tabs.create({
                     index: tab.index + 1,
                     url: image
                 });
             });
         } else if(confirm(chrome.i18n.getMessage('failure'))) {
-            var xhr, url = encodeURIComponent(info.frameUrl || info.pageUrl);
-            xhr = new XMLHttpRequest();
+            const url = encodeURIComponent(info.frameUrl || info.pageUrl);
+            const xhr = new XMLHttpRequest();
             xhr.open('GET', 'https://script.google.com/macros/s/AKfycbwtacVYsZHc_n4qkVdfvjJE_3__rKwfeqTNNgZKPhH00VoKTAA/exec?url=' + url);
             xhr.send();
         }

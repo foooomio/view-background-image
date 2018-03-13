f51d04e71e9b7afe20c0513e17eb2864eb83a15e
foooomio <foooomio@gmail.com>
2018-03-13 19:10:00 +0900

    Alert if content script not loaded

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
-        contexts: ['page', 'frame', 'selection', 'link', 'editable',  'image', 'video', 'audio'],
+        contexts: ['page', 'frame', 'selection', 'link', 'editable', 'image'],
         id: 'background_img',
         title: chrome.i18n.getMessage('title')
     });
@@ -14,18 +14,28 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.tabs.sendMessage(tab.id, '', response => {
-        if (response) {
-            response.split(' ').map(image => {
+
+        if (response === undefined) {
+            alert(chrome.i18n.getMessage('reload'));
+            return;
+        }
+
+        if (response === '' || typeof response !== 'string') {
+            if (confirm(chrome.i18n.getMessage('failure'))) {
                 chrome.tabs.create({
                     index: tab.index + 1,
-                    url: image
+                    url: 'https://github.com/foooomio/view-background-image/issues'
                 });
-            });
-        } else if(confirm(chrome.i18n.getMessage('failure'))) {
+            }
+            return;
+        }
+
+        response.split(' ').map(image => {
             chrome.tabs.create({
                 index: tab.index + 1,
-                url: 'https://github.com/foooomio/view-background-image/issues'
+                url: image
             });
-        }
+        });
+
     });
 });

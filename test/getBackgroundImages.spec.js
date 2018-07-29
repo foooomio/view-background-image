386af5904349c02a89117a7ce42da83eb7fbbd2a
foooomio <foooomio@gmail.com>
2018-07-29 23:25:12 +0900

    Add automation testing

---
+++ test/getBackgroundImages.spec.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const launchOptions = {
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+};
+
+const testCaseDir = path.resolve(__dirname, 'html');
+const contentScript = path.resolve(__dirname, '../src/content.js');
+
+describe('getBackgroundImages', function() {
+    let browser, page;
+
+    before(async function() {
+        browser = await puppeteer.launch(launchOptions);
+        page = await browser.newPage();
+    });
+
+    after(async function() {
+        await browser.close();
+    });
+
+    const expected = ['blue.png'];
+    const point = { x: 50, y: 50 };
+
+    fs.readdirSync(testCaseDir).forEach(filename => {
+        it(filename, async function() {
+            await page.goto('file://' + path.resolve(testCaseDir, filename));
+            await page.addScriptTag({ path: contentScript });
+
+            const results = await page.evaluate(
+                (x, y) => document.getBackgroundImages(x, y), point.x, point.y
+            );
+
+            const actual = results.map(r => path.basename(r));
+            assert.deepStrictEqual(actual, expected);
+        });
+    });
+});

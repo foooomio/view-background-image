669aba5857c86067535a2877831f9660a5e9739f
foooomio <foooomio@gmail.com>
2018-10-04 04:04:44 +0900

    Add test for locale json

---
+++ test/locales.spec.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+describe('locales', function() {
+    const locales = path.resolve(__dirname, '../src/_locales');
+    const keys = [
+        'extName', 'extDescription', 'title', 'reload', 'failure'
+    ];
+
+    fs.readdirSync(locales).forEach(locale => {
+        it(locale, function() {
+            const file = fs.readFileSync(path.resolve(locales, locale, 'messages.json'));
+            const json = JSON.parse(file);
+            keys.forEach(key => {
+                assert(typeof json[key].message === 'string');
+            });
+        });
+    });
+});

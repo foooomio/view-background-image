a46fb86062713981ee3b86eaad2166f0d20f6705
foooomio <foooomio@gmail.com>
2018-10-10 22:38:45 +0900

    Improve build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+NAME="view-background-image"
+VERSION=$(jq -r .version src/manifest.json)
 
 mkdir -p dist
 rm dist/*.zip
 
 # for Chrome
-zip -r dist/chrome.zip src -x *.DS_Store
+zip -r "dist/${NAME}-${VERSION}.zip" src -x "*.DS_Store"

ba0079ecaa65ba2d83fb43fe4baaa3d3d5c84a4b
foooomio <foooomio@gmail.com>
2024-06-14 03:18:42 +0900

    Fix bug in Incognito mode

--- manifest.sh
+++ manifest.sh
@@ -5,12 +5,14 @@ set -euo pipefail
 case "$1" in
   'chrome')
     jq '.background|={"service_worker": "background.js"}' < src/manifest.json | \
+    jq '.incognito|="split"' | \
     jq 'del(.browser_specific_settings)' > tmp/manifest.json
     mv tmp/manifest.json src/manifest.json
     yarn prettier --write src/manifest.json
     ;;
   'firefox')
     jq '.background|={"scripts": ["background.js"]}' < src/manifest.json | \
+    jq 'del(.incognito)' | \
     jq '.browser_specific_settings|={"gecko":{"id":"{f1f7b448-d666-46c6-9dfa-3ee4c5c83578}"}}' > tmp/manifest.json
     mv tmp/manifest.json src/manifest.json
     yarn prettier --write src/manifest.json

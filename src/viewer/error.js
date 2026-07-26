a274ac46e86c18f7cbbd8d47161c0f664351b8a1
foooomio <foooomio@gmail.com>
2026-07-27 01:45:02 +0900

    Update viewer

--- src/viewer/error.js
+++ src/viewer/error.js
@@ -6,9 +6,4 @@ import { $ } from './utils.js';
 export function showError(error) {
   $('error-details').textContent = error.toString();
   $('error').classList.remove('hidden');
-  $('nav').classList.add('hidden');
-
-  if (navigator.userAgent.includes('Firefox')) {
-    $('error-firefox').classList.remove('hidden');
-  }
 }
